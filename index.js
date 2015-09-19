b67c24178ba3a8346ab4fb88de6790d01e41611a
Alexey Komarov <alex7kom@gmail.com>
2015-09-19 15:30:15 +0300

    Improved code style

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ var cheerio = require('cheerio');
 
 var steamDomain = 'https://steamcommunity.com';
 
-module.exports = function getAPIKey(options, callback) {
+module.exports = function getAPIKey (options, callback) {
   var cookieJar = request.jar();
   var _request = request.defaults({ jar: cookieJar });
 
-  options.webCookie.forEach(function(name) {
+  options.webCookie.forEach(function (name) {
     cookieJar.setCookie(request.cookie(name), steamDomain);
   });
 
   _request.get({
     uri: steamDomain + '/dev/apikey'
-  }, function(error, response, body) {
-    if (error || response.statusCode != 200) {
+  }, function (error, response, body) {
+    if (error || response.statusCode !== 200) {
       return callback(error || new Error(response.statusCode));
     }
 
     var $ = cheerio.load(body);
 
-    if ($('#mainContents h2').html() == 'Access Denied') {
+    if ($('#mainContents h2').html() === 'Access Denied') {
       return callback(new Error('Access Denied'));
     }
 
-    if ($('#bodyContents_ex h2').html() == 'Your Steam Web API Key') {
+    if ($('#bodyContents_ex h2').html() === 'Your Steam Web API Key') {
       var key = $('#bodyContents_ex p').html().split(' ')[1];
       return callback(null, key);
     }
@@ -37,7 +37,7 @@ module.exports = function getAPIKey(options, callback) {
         sessionid: options.sessionID,
         submit: 'Register'
       }
-    }, function(error, response, body) {
+    }, function () {
       getAPIKey(options, callback);
     });
   });
